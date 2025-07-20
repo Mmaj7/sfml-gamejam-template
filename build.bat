e3debd784588b824842ba54422fa8f938ac60aed
Maxim <106436366+Mmaj7@users.noreply.github.com>
2025-07-20 12:54:42 +0300

    add build.bat

---
+++ build.bat
@@ -0,0 +1,4 @@
+"C:\mingw64\bin\g++" -std=c++17 -static main.cpp -o main.exe -IC:\SFML-2.6.2\include -LC:\SFML-2.6.2\lib -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -mwindows
+
+pause
+
